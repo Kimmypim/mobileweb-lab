152aa26dc5c330e713b9b15692fa385048971440
Kimmypim <pimapsorn81047@gmail.com>
2026-04-10 02:38:21 +0700

    lab8

---
+++ apps/lab08-ai/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cskku.pimapsorn.lab08',
+  appName: 'lab08-ai',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ apps/lab08-ai/src/core/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJsanc6AoJsVmtKNFTqEdq3kduW8e7b6I",
+  authDomain: "mobileapp-fe3bc.firebaseapp.com",
+  projectId: "mobileapp-fe3bc"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
